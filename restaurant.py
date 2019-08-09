1e52ac7fade5e471744aae84364f516164335afe
Nadeem Akhtar <42943800+akhtarnadeem915@users.noreply.github.com>
2019-08-09 17:33:36 +0530

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -18,7 +18,7 @@ data['Reservation'],_ = pd.factorize(data['Reservation'])
 data['Waiting'],_ = pd.factorize(data['Waiting'])
 
 X = data[feature_cols]
-y = data['Wait']
+y = data['Decision']
 
 #Create the instance of DecisionTreeClassifier with max_depth = None i.e till leaf node & random_state=0 for maintaining state
 tree_clf = DecisionTreeClassifier(max_depth=None, random_state=1)
@@ -27,15 +27,15 @@ tree_clf = DecisionTreeClassifier(max_depth=None, random_state=1)
 tree_clf.fit(X, y)
 
 #input from user
-print("1 = Yes, 0 = No")
-alternate = int(input("Alternate: "))
-bench = int(input("Bench: "))
-hungry = int(input("Hungry: "))
-rain = int(input("Rain: "))
-waiting = int(input("Waiting: "))
-costly = int(input("Costly: "))
-reservation = int(input("Reservation: "))
-patrons = int(input("Patrons: "))
+print("enter 1 = Yes, 0 = No")
+alternate = int(input("Is there any Alternate restaurant: "))
+bench = int(input("Is there Bench to wait: "))
+hungry = int(input("Are you Hungry: "))
+rain = int(input("Is it Raining: "))
+waiting = int(input("Is there Waiting: "))
+costly = int(input("Is it Costly: "))
+reservation = int(input("Do you have a Reservation: "))
+patrons = int(input("Are there Patrons: "))
 #predict
 y_predict = tree_clf.predict([[alternate,bench,hungry,waiting,costly,rain,reservation,patrons]])
 print("As per situaution it is better to",y_predict)
@@ -43,8 +43,8 @@ print("As per situaution it is better to",y_predict)
 #visualization
 dot_data = export_graphviz(tree_clf, out_file = None, 
 	filled = True, rounded = True,	special_characters = True, 
-	feature_names = feature_cols, class_names = data['Wait'])
+	feature_names = feature_cols, class_names = data['Decision'])
 graph = pydotplus.graph_from_dot_data(dot_data)
-graph.write_png('restaurant.png')
-decisionTree = Image.open('restaurant.png')
+graph.write_png('output-tree.png')
+decisionTree = Image.open('output-tree.png')
 decisionTree.show()
